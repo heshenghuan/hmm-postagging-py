8f690bc425874390fd6a09f6533752d546726337
heshenghuan <heshenghuan@sina.com>
2016-05-06 11:53:45 +0800

    add calculate start probability func

--- pretreatment.py
+++ pretreatment.py
@@ -8,6 +8,7 @@ http://github.com/heshenghuan
 """
 
 import codecs
+import math
 import sys
 
 TAGSET = set()
@@ -99,6 +100,52 @@ def saveHMM(INIT_PROBFILE='ctb7_init.txt', TRAN_PROBFILE='ctb7_tran.txt',
     i_out.close()
     f_out.close()
     print "Finished."
+
+
+def calcInitProb(INIT_PROBFILE='ctb7_init.txt', smooth=0):
+    """
+    :param
+    """
+    i_input = codecs.open(INIT_PROBFILE, mode='r', encoding='utf8')
+    i_out = codecs.open('ctb7_initprob.txt')
+    print "Calculate the start probability."
+    is_first_line = True
+    freq_sum = 0
+    tag_freq = {}
+    for line in i_input.readlines():
+        if is_first_line:
+            is_first_line = False
+            continue
+        raw = line.strip().split()
+        tag_freq[raw[0]] = int(raw[1])
+        freq_sum += int(raw[1])
+
+    print "Writing start log_probability to the file."
+    i_out.write("#INIT_PROB log_probability\n")
+    for tag in tag_freq.keys():
+        i_out.write(tag + ' ')
+        if smooth == 0:
+            # No smoothing tech used
+            if tag_freq[tag] == 0:
+                i_out.write('-inf\n')
+            else:
+                freq = float(tag_freq[tag])
+                i_out.write('%.4f\n' % (math.log(freq / freq_sum)))
+        elif smooth == 1:
+            # Laplace smoothing
+            freq = tag_freq[tag]
+            log_p = math.log((freq + 1.0) / (freq_sum + 37.0))
+            i_out.write('%.4f\n' % log_p)
+        elif smooth == 2:
+            # Good-Turing smoothing
+
+
+def calcLogProb(INIT_PROBFILE='ctb7_init.txt', TRAN_PROBFILE='ctb7_tran.txt',
+                TAGFREQFILE='ctb7_tagFreq.txt', WORDFREQFILE='ctb7_words.txt'):
+    """
+    """
+    return 
+
 
 if __name__ == '__main__':
     getHmmModelInfo()
